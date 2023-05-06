8ddf923aa4181330e4f621a003c4aff486a5ed44
Eddie <edikbraga80@gmail.com>
2023-05-06 13:21:40 +0300

    no message

---
+++ parser.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+
+rddm = 'https://xn--90acagbhgpca7c8c7f.xn--p1ai/news'
+unarmy = 'https://yunarmy.ru/yunarmeyskielagerya/'
+
+page1 = requests.get(rddm)
+page2 = requests.get(unarmy)
+
+print(page1.status_code)
+print(page2.status_code)
+
+filteredNews = []
+allNews = []
+
+soup1 = BeautifulSoup(page1.text, 'html.parser')
+#soup2 = BeautifulSoup(page2.text, 'html.parser')
+
+print(soup1)
+#print(soup2)
+
+allNews = soup1.findAll('div', class_='news-page__container')
+
+for data in allNews:
+    if data.find('p', class_='news-card__text') is not None:
+        filteredNews.append(data.text)
+
+for data in filteredNews:
+    print(data)
+
+print(filteredNews)
