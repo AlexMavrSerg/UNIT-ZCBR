32b0dfc72b813b9816ff6dec38dc4f753e864ad9
Tolandorado <tonisagaz@mail.ru>
2023-05-27 12:55:48 +0300

    начало работы над блоком профиля

---
+++ front-end/src/component/Profile/profile.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import '../Profile/profile.scss';
+import { useEffect } from 'react';
+
+const Profile = () => {
+    const [profile, setProfile] = useState('');
+    
+
+}
