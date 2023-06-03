c6c109d4bbeb9dabc5d7e3b1cafa6e1f70c54629
Tolandorado <tonisagaz@mail.ru>
2023-06-03 10:41:12 +0300

    profile modif.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,11 @@
 import Form from "./component/Form/form";
-
+import ProfileSidebar from "./component/Profile/profile";
 
 function App() {
   return (
     <div className="App">
       <Form/>
+      <ProfileSidebar/>
     </div>
   );
 }
--- front-end/src/component/Profile/profile.jsx
+++ front-end/src/component/Profile/profile.jsx
@@ -1,9 +1,58 @@
 import { useState } from 'react';
 import '../Profile/profile.scss';
-import { useEffect } from 'react';
+import sidebar from '../../UI-img/sidebar-arrow.svg';
 
-const Profile = () => {
-    const [profile, setProfile] = useState('');
-    
+// const Profile = () => {
+//     const [profile, setProfile] = useState(false);
+//     const [client, setClient] = useState(false);
+//     const []
 
-}
+// }
+
+function ProfileSidebar() {
+    const [isOpen, setIsOpen] = useState('');
+
+  
+    const handleToggle = () => {
+      setIsOpen(!isOpen);
+    };
+  
+
+  
+    return (
+      <div className='profile-container'>
+
+        <div className={`profile-sidebar ${isOpen ? 'open' : ''}`}>
+
+
+<div className="client">
+  <div className="client-icon"> 
+       {/*  <img src="" alt="" /> */}
+  </div>
+  <div className="client-info">
+    <span className="client-name">Антон Чигур</span>
+    <span className="client-birthday">11 11 1111</span>
+    <span className="client-id">DWIN: Gorter</span>
+    <span className="client-supp">Supporters 9999</span>
+  </div>
+  
+
+</div>
+
+<div className="profile-stickers"></div>
+
+<div className="profile-stickers"></div>
+
+      </div>
+
+      <div className="toggle-button" onClick={handleToggle}>
+       <img src={sidebar} alt="" className='profile-sidebar__arrow'/>
+       <img src={sidebar} alt="" className='profile-sidebar__arrow'/>
+       <img src={sidebar} alt="" className='profile-sidebar__arrow'/>
+        </div>
+
+      </div>
+    );
+  }
+  
+  export default ProfileSidebar;
