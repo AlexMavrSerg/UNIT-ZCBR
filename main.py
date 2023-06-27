cd2c06636d8859b32f8d4e0d3259a97316a928f2
nayrest <67153499+nayrest@users.noreply.github.com>
2023-06-27 13:55:16 +0300

    Convert json to HTML with dowwnloading .json file

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import json
+import ftplib
 from json2html import *
 
-with open("D:\display-html\output.json", encoding="utf-8") as f:
+path = '/vita-dev.com/public_html'
+filename = 'output.json'
+ftp = ftplib.FTP("e97891sb.beget.tech")
+ftp.login("e97891sb_nayrest", "i4tpx70As!")
+ftp.cwd(path)
+ftp.retrbinary("RETR " + filename, open(filename, 'wb').write)
+ftp.quit()
+
+with open("output.json", encoding="utf-8") as f:
     d = json.load(f)
     scanOutput = json2html.convert(json=d)
-    htmlReportFile = "D:\display-html\index.html"
+    htmlReportFile = r"D:\Web Development\json2html\UNIT-ZCBR\index.html"
     with open(htmlReportFile, 'w', encoding='utf-8') as htmlfile:
         htmlfile.write(str(scanOutput))
         print("Json file converted into html successfully.")
